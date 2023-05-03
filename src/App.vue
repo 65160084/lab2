79f91e43ecadfc1185aa7299389c6bb4dfeb51d2
Siwakorn Chotipanang <65160084@go.buu.ac.th>
2023-05-04 00:56:34 +0700

    Finish Attribute Bindings

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 import CalComponent from './components/CalComponent.vue';
 import HelloWorldExample from './components/HelloWorldExample.vue';
 import HandlingUserInput from './components/HandlingUserInput.vue';
+import AttributeBindings from './components/AttributeBindings.vue';
 </script>
 
 <template>
   <div>
+    <AttributeBindings />
     <HandlingUserInput />
     <CalComponent />
     <HelloWorldExample />
