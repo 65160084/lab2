3708a3b0ad1c27f3c0bbb2e4bc8816fba03780c6
Siwakorn Chotipanang <65160084@go.buu.ac.th>
2023-05-04 00:31:14 +0700

    Begining attributebindings

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import CalComponent from './components/CalComponent.vue';
 import HelloWorldExample from './components/HelloWorldExample.vue';
+import HandlingUserInput from './components/HandlingUserInput.vue';
 </script>
 
 <template>
   <div>
+    <HandlingUserInput />
     <CalComponent />
     <HelloWorldExample />
   </div>
---
+++ src/components/HandlingUserInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+const message = ref("Hello world!");
+function reverseMessage() {
+    message.value = message.value.split("").reverse().join("");
+}
+function notify() {
+    alert("Navigation was prevented.");
+}
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <button @click="reverseMessage">Reverse Messgae</button>
+    <button v-on:click="message += '!'">Append "!"</button>
+    <a href="้https://vusjs.org" @click.prevent="notify">A link with e.prevendDefault</a>
+</template>
+
+<style scoped>
+button, a{
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
