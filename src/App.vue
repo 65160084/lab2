428018cc6799231b2aeeeb9fa0600347eec84790
Siwakorn Chotipanang <65160084@go.buu.ac.th>
2023-05-04 21:18:29 +0700

    Finish ConditionsAndLoops

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,14 @@ import CalComponent from './components/CalComponent.vue';
 import HelloWorldExample from './components/HelloWorldExample.vue';
 import HandlingUserInput from './components/HandlingUserInput.vue';
 import AttributeBindings from './components/AttributeBindings.vue';
+import ConditionsAndLoops from './components/ConditionsAndLoops.vue';
 </script>
 
 <template>
   <div>
+    <ConditionsAndLoops />
+    <br />
+    <br />
     <AttributeBindings />
     <HandlingUserInput />
     <CalComponent />
